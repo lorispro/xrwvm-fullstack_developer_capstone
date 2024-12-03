0b7fc8825a3de74b35c82513f5e614078213b85e
lorispro <l9600@live.it>
2024-12-03 11:32:56 -0500

    third commit

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):  # Use TabularInline for a table-like display
+    model = CarModel
+    extra = 1  # Number of extra empty forms displayed
+    
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'dealer_id', 'car_type', 'year')  # Corrected to use car_type
+    list_filter = ('car_type', 'year', 'car_make')  # Corrected to use car_type
+    search_fields = ('name', 'car_make__name')  # Enable search by car model or make
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'country_of_origin')
+    search_fields = ('name',)
+    inlines = [CarModelInline]  # Link CarModelInline for managing CarModel inside CarMake
 
-# Register models here
+# Register models
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,34 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+# CarMake model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)  # Name of the car make
+    description = models.TextField()  # Description of the car make
+    country_of_origin = models.CharField(max_length=100, blank=True, null=True)  # Optional field for country of origin
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name  # String representation of the car make
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# CarModel model
+class CarModel(models.Model):
+    # Choices for car type
+    CAR_TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+    ]
+
+    name = models.CharField(max_length=100)  # Name of the car model
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-one relationship with CarMake
+    car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)  # Type of the car
+    year = models.IntegerField(
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)]  # Year validation
+    )
+    dealer_id = models.IntegerField()  # ID of the dealer (optional field for dealer reference)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"  # String representation of the car model
